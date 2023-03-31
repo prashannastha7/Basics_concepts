bc64d81df00aeaf110e110a4758f74a73f2020e5
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-01 00:01:23 +0545

    Callbacks

--- PracticeJS/ForNode.js
+++ PracticeJS/ForNode.js
@@ -1,1 +1,22 @@
 //Callback function -> is a function passed into another function as an argument, which is then invoked inside the outer function to complete an action
+/*function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+  }
+  
+  myCalculator(5, 5, myDisplayer);
+
+// In the example below, myFunction is used as a callback.
+//myFunction is passed to setTimeout() as an argument.
+
+setTimeout(myFunction, 3000);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "I love You !!";
+}*/
+
+//Promises -> 
