15963b939b0bee5ad9dcbabdde51f89b4b4b0bdd
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-18 21:44:35 +0545

    path & fs module

--- PractiseNode/path.js
+++ PractiseNode/path.js
@@ -6,3 +6,13 @@ const a3 = path.extname(__filename)
 console.log(a)
 console.log(a3)
 
+//FS module 
+
+const fs = require('fs')
+
+const x = fs.readFileSync('file.txt')
+console.log(x.toString())
+console.log("Finished reading file")
+
+const y = fs.writeFileSync('file.txt', "you are learning.")
+console.log(y)
