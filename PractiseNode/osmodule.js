ef55034b3d11a840351138bed74f1fa87c0ed44f
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-18 19:14:06 +0545

    Os module

---
+++ PractiseNode/osmodule.js
@@ -0,0 +1,9 @@
+const os = require('os');  //buit-in module
+
+
+//from node docs
+console.log(os.freemem())
+console.log(os.homedir())
+console.log(os.hostname())
+console.log(os.uptime())
+console.log(os.totalmem())
