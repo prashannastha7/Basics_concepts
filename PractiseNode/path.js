d58f165322422290ea3018cc294c8cf6b8b88212
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-18 19:26:06 +0545

    path & fs module

---
+++ PractiseNode/path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const a = path.basename('A:\\espanol.txt')
+const a2 = path.dirname('A:\\espanol.txt')
+const a3 = path.extname(__filename)
+console.log(a)
+console.log(a3)
+
