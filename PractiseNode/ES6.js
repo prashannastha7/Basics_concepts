be1fdc1b9bdbaadf0abca2194327252257a00961
prashannastha7 <prashannabdrshrestha711@gmail.com>
2023-04-19 15:21:10 +0545

    ES6

---
+++ PractiseNode/ES6.js
@@ -0,0 +1,10 @@
+//add type: module in package.json
+
+//import{simple1 as simple} from "./fES6.mjs"
+//import simple69 from "./fES6.mjs" //for single only
+//import{simple, simple1} from "./fES6.mjs"
+import * as a2 from "./fES6.mjs"
+//simple69() // defualt will pass
+
+console.log(a2)
+console.log(a2.simple())
---
+++ PractiseNode/fES6.mjs
@@ -0,0 +1,8 @@
+export function simple(){
+    console.log("Passing this")
+
+}
+
+export default function simple1(){
+    console.log("Passing this to ")
+}
